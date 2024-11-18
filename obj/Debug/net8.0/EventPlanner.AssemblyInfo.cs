7c7a26c7e176a470b793c87f25afa890a1e9354a
notjuan01 <mjuanpablo523@gmail.com>
2024-11-18 10:56:52 -0500

    Se crean las carpetas

--- obj/Debug/net8.0/EventPlanner.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventPlanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27a239be84d402717d593c80749edf0397f1169")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
